2b899b2360ea82ea34cdd39a145746ddbe48a55e
Guilherme C Pereira <guilhermecarvalho1144@gmail.com>
2026-02-11 20:26:47 -0300

    add logger

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 import streamlit as st
 import requests
+import logging
+
+logging.basicConfig(level=logging.INFO, filemode="w")
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler("app.log")
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 API_URL = st.sidebar.text_input("API URL", value="http://localhost:8000")
 
@@ -24,6 +34,7 @@ if prompt := st.chat_input("Ask something"):
         data = response.json()["reply"]
     except Exception as e:
         data = f"Backend error: {e}"
+        logger.error("Error while calling backend: %s", e)
     with st.chat_message("assistant"):
         st.markdown(data)
     st.session_state.messages.append({"role": "assistant", "content": data})
--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,27 @@
 from fastapi import FastAPI
+import logging
+
+logging.basicConfig(level=logging.INFO, filemode="a")
+
+logger = logging.getLogger(__name__)
+handeler = logging.FileHandler("backend.log")
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+handeler.setFormatter(formatter)
+logger.addHandler(handeler)
+
 
 app = FastAPI()
 
 
 @app.get("/health")
 async def health():
+    logger.info("Health check endpoint was called.")
     return {"status": "ok"}
 
 
 @app.get("/echo")
 async def echo(message: str):
+    logger.info("Echo endpoint was called with message: %s", message)
     return {"reply": f"You said {message}"}
