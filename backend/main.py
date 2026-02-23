fbd1ad768973cd4d63e3e9c5c2c47456c7a097a4
Guilherme C Pereira <guilhermecarvalho1144@gmail.com>
2026-02-22 23:29:13 -0300

    add inngest in a simple echo function

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import logging
+import inngest
+import inngest.fast_api
 
 logging.basicConfig(level=logging.INFO, filemode="a")
 
@@ -11,8 +13,30 @@ formatter = logging.Formatter(
 handeler.setFormatter(formatter)
 logger.addHandler(handeler)
 
+inngest_client = inngest.Inngest(
+    app_id="local-llm-obsedian-rag",
+    logger=logging.getLogger("uvicorn"),
+    is_production=False,
+    serializer=inngest.PydanticSerializer(),
+)
+
+
+@inngest_client.create_function(
+    fn_id="echo_function", trigger=inngest.TriggerEvent(event="/echo")
+)
+async def echo_function(ctx: inngest.Context) -> dict:
+    return {"reply": "Hello from the echo function!"}
+
 
 app = FastAPI()
+
+inngest.fast_api.serve(
+    app,
+    inngest_client,
+    [
+        echo_function,
+    ],
+)
 
 
 @app.get("/health")
